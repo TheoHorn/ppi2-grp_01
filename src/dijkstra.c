0b30654f955a8ef5731d7ad85ed5d40d2a56ac5b
TEMPESTINI Terry <terry.tempestini@telecomnancy.eu>
2023-04-08 22:58:00 +0200

    path_improvements

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -1,10 +1,7 @@
 #include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <float.h>
-#include <limits.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include "station.h"
 #include "parser_csv.h"
@@ -14,7 +11,6 @@
 #define dmin 0
 #define ININITY 999999
 
-// return the minimum distance between two station in km
 int dijkstra(station_t depart, station_t arrivee, station_t *stations, int nbStations, int *predecessors){
     int* distances = calloc(nbStations, sizeof(int));
 
@@ -29,7 +25,7 @@ int dijkstra(station_t depart, station_t arrivee, station_t *stations, int nbSta
 
     for(int i = 0; i < nbStations; i++){
         visited[current_station] = 1;
-        int* voisines = stationVoisines(start, visited, dmax, stations, nbStations);
+        int* voisines = adjacentStations(start, visited, dmax, stations, nbStations);
         updateNeighbors(start, voisines, distances, stations, predecessors);
 
         int minValue = min_distance_index(distances, visited, nbStations);
@@ -54,7 +50,6 @@ int dijkstra(station_t depart, station_t arrivee, station_t *stations, int nbSta
     return resultat;
 }
 
-// station = station de départ ; voisines = tableau des stations voisines
 void updateNeighbors(station_t* station, int* voisines, int* distances, station_t* stations, int* predecessors){
     int i = 0;
     while(voisines[i] != -1){
@@ -69,7 +64,7 @@ void updateNeighbors(station_t* station, int* voisines, int* distances, station_
     }
 }
 
-int *stationVoisines(station_t* station, int* visited, int distanceMax, station_t *stations, int nbStations){
+int *adjacentStations(station_t* station, int* visited, int distanceMax, station_t *stations, int nbStations){
     int *voisines = malloc(sizeof(int) * nbStations);
     int nbVoisines = 0;
     for(int i = 0; i < nbStations; i++){
@@ -101,34 +96,71 @@ int min_distance_index(int *distances, int *visited, int n){
     return index;
 }
 
+int predecessorsToPath(int* predecessors, int* path, int arrivee){
+    int j=1;
+    path[0]=arrivee;
+    while(predecessors[arrivee] != -1){
+        path[j] = predecessors[arrivee];
+        arrivee = predecessors[arrivee];
+        j++;
+    }
+    return j-1;
+}
+
+void displayPath(int* path, int length){
+    for(int k = length; k >= 1; k--){
+        printf("%d-->", path[k]);
+    }
+    printf("%d", path[0]);
+}
+
+int decreaseNumberStations(station_t* stations ,int*path, int*newPath, int length, int autonomy){
+    int currentStation = path[0];
+    newPath[0] = path[0];
+    for(int i = 1; i < length; i++){
+        while(autonomy - distance(&stations[currentStation], &stations[path[i]]) > 0 && i < length){
+            autonomy = autonomy - distance(&stations[currentStation], &stations[path[i]]);
+            i++;
+        }
+    }
+
+    for(int i = 0; i < length; i++){
+        //printf("%d ", newPath[i]);
+    }
+
+    return length;
+}
+
 int main() {
+    int depart = 1;
+    int arrivee = 2200;
+
     csv_reader_t reader = create_reader_default(DATASET_PATH_STATIONS);
     station_t stations[DATASET_STATIONS_LINES];
     assert(parse_to_station(&reader, stations) == 0);
-    
-    int n = sizeof(stations) / sizeof(stations[0]); // number of stations
 
-    int* predecessors = calloc(n, sizeof(int));
-
-    int depart = 1;
-    int arrivee = 2200;
+    int* predecessors = calloc(DATASET_STATIONS_LINES, sizeof(int));
    
     clock_t t=0;
-    int distMin = dijkstra(stations[depart], stations[arrivee], stations, n, predecessors);
+    int distMin = dijkstra(stations[depart], stations[arrivee], stations, DATASET_STATIONS_LINES, predecessors);
     t = clock() - t;
 
     printf("\nDistance min between station %d and station %d: %d km\n",depart, arrivee, distMin);
     printf("Path: ");
-    int i = arrivee;
-    int d=0;
-    while(predecessors[i] != -1){
-        d += distance(&stations[i], &stations[predecessors[i]]);
-        printf("%d <- ", i);
-        i = predecessors[i];
-    }
-    printf("%d\nDistance directe = %d km", i, d);
+
+    int* path = malloc(sizeof(int) * DATASET_STATIONS_LINES);
+    int length = predecessorsToPath(predecessors,path,arrivee);
+
+    int* newPath = malloc(sizeof(int) * DATASET_STATIONS_LINES);
+    int newLength = decreaseNumberStations(stations, path, newPath, length, 200);
+    free(newPath);
+
+    displayPath(path, length);
+
+    printf("\nDistance directe = %d km", distance(&stations[depart], &stations[arrivee]));
     printf("\nTime: %fs\n", ((double)t)/CLOCKS_PER_SEC);
 
+    free(path);
     free(predecessors);
     free_parsed_station(stations);
 
--- src/dijkstra.h
+++ src/dijkstra.h
@@ -11,12 +11,18 @@ int dijkstra(station_t depart, station_t arrivee, station_t *stations, int nbSta
 int min_distance_index(int *distances, int *visited, int n);
 
 // returns all the stations that are at a distance of distanceMax from the station
-int *stationVoisines(station_t* station, int* visited, int distanceMax, station_t *stations, int nbStations);
+int *adjacentStations(station_t* station, int* visited, int distanceMax, station_t *stations, int nbStations);
 
 // free the array of station returned by stationVoisines
 void freeVoisines(int *voisines);
 
 // update the distances of the neighbors of the station
 void updateNeighbors(station_t* station, int* voisines, int* distances, station_t* stations, int* predecessors);
+
+// returns the path from the depart to the arrivee
+int predecessorsToPath(int* predecessors, int* path, int arrivee);
+
+// display the path
+void displayPath(int* path, int length);
 
 #endif
